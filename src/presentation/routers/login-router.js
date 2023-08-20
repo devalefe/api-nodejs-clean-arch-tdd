096e3f4d5eb8f62aad3e420397f62f1f65d44e3c
Alefe F Oliveira <dev.alefe@gmail.com>
2023-08-20 20:16:43 -0300

    move classes to its own file

---
+++ src/presentation/routers/login-router.js
@@ -0,0 +1,16 @@
+const HttpResponse = require('../helpers/http-response')
+
+module.exports = class LoginRouter {
+  route (httpResquest) {
+    if (!httpResquest || !httpResquest.body) {
+      return HttpResponse.serverError()
+    }
+    const { email, password } = httpResquest.body
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+    if (!password) {
+      return HttpResponse.badRequest('password')
+    }
+  }
+}
