a9f35bddf520fc7bbdcee6186b48c6c183eb303f
Alefe F Oliveira <dev.alefe@gmail.com>
2023-08-20 18:41:38 -0300

    ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpResquest) {
-    if (!httpResquest.body.email) {
+    if (!httpResquest.body.email || !httpResquest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,17 @@ describe('Login Router', () => {
     const httpResquest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpResquest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpResquest = {
+      body: {
+        email: 'any_email@email.com'
       }
     }
     const httpResponse = sut.route(httpResquest)
