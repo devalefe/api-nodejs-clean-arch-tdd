fae5552381415d48077fa17be1857767e25a860a
Alefe F Oliveira <dev.alefe@gmail.com>
2023-08-20 23:00:22 -0300

    ensure LoginRouter returns a valid token if valid credentials are provided

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -2,9 +2,10 @@ const MissingParamError = require('./missing-param-error')
 const UnauthorizedError = require('./unauthorized-error')
 
 module.exports = class HttpResponse {
-  static ok () {
+  static ok (data) {
     return {
-      statusCode: 200
+      statusCode: 200,
+      body: data
     }
   }
 
