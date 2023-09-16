68db5638a67b40ac4afc29bb4feba7682bb11830
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-16 10:07:54 -0300

    refactor: move LoadUserByEmailRepository to its own file

---
+++ src/infra/repositories/load-user-by-email-repository.js
@@ -0,0 +1,16 @@
+module.exports = class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async load (email) {
+    const user = await this.userModel.findOne({
+      email
+    }, {
+      projection: {
+        password: true
+      }
+    })
+    return user
+  }
+}
