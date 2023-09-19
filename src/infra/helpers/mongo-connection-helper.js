dbf8dc62fc8d4a889c86bc49791c43d72e4768df
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-19 19:57:08 -0300

    refactor: remove dbName from MongoHelper

--- src/infra/helpers/mongo-connection-helper.js
+++ src/infra/helpers/mongo-connection-helper.js
@@ -1,11 +1,10 @@
 const { MongoClient } = require('mongodb')
 
 module.exports = {
-  async connect (uri, dbName) {
+  async connect (uri) {
     this.uri = uri
-    this.dbName = dbName
     this.client = await MongoClient.connect(uri)
-    this.db = await this.client.db(dbName)
+    this.db = await this.client.db()
   },
   async disconnect () {
     await this.client.close()
