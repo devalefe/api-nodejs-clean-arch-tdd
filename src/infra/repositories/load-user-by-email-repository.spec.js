30e329a4a83d3ec5ecf6ed22a2020e72c2eceb34
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-19 12:32:42 -0300

    chore: should throw if not email is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,3 +1,4 @@
+const MissingParamError = require('../../utils/errors/missing-param-error')
 const MongoHelper = require('../helpers/mongo-connection-helper')
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
 let db
@@ -52,5 +53,11 @@ describe('LoadUserByEmail Repository', () => {
     const sut = new LoadUserByEmailRepository()
     const promise = sut.load('any_email@mail.com')
     expect(promise).rejects.toThrow()
+  })
+
+  test('Should throw if no email is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.load()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
