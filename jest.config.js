3b2ea9715d55364bcd29d45fee47f68549ffe25b
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-19 15:36:13 -0300

    ignore main module on test coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const config = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
   preset: '@shelf/jest-mongodb',
   watchPathIgnorePatterns: ['globalConfig']
 }
