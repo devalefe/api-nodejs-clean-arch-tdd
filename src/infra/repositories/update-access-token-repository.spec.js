1fa17c9924aec4140023aa71ff272af0780c39b8
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-19 13:06:33 -0300

    chore: ensure UpdateAccessTokenRepository update user with correct accessToken

---
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -0,0 +1,46 @@
+const MongoHelper = require('../helpers/mongo-connection-helper')
+let db
+
+class UpdateAccessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async update (userId, accessToken) {
+    await this.userModel.updateOne({
+      _id: userId
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+  }
+}
+
+describe('UpdateAccessToken Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(globalThis.__MONGO_URI__, globalThis.__MONGO_DB_NAME__)
+    db = await MongoHelper.getDb()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should user access token with correct values', async () => {
+    const userModel = db.collection('users')
+    const sut = new UpdateAccessTokenRepository(userModel)
+    const user = await userModel.insertOne({
+      name: 'Any Name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
+    await sut.update(user.insertedId, 'valid_token')
+    const updatedUser = await userModel.findOne({ _id: user.insertedId })
+    expect(updatedUser.accessToken).toBe('valid_token')
+  })
+})
