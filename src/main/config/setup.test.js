1c92f1c2f18909c01bac25eca01d8fff58610ded
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-19 14:11:28 -0300

    chore: enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -3,10 +3,20 @@ const app = require('./app')
 
 describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
-    app.get('/x-powered-by', (req, res) => {
+    app.get('/test_x-powered-by', (req, res) => {
       res.send()
     })
-    const response = await request(app).get('/x-powered-by')
+    const response = await request(app).get('/test_x-powered-by')
     expect(response.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+    const response = await request(app).get('/test_cors')
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+    expect(response.headers['access-control-allow-methods']).toBe('*')
+    expect(response.headers['access-control-allow-headers']).toBe('*')
   })
 })
