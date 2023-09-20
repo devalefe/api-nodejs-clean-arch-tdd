bd0c47434cc366b4b1fa577943860461881b87cb
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-19 21:35:18 -0300

    chore: ensure login route returns 200 if valid credentials are provided

---
+++ src/main/routes/login-routes.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest')
+const app = require('../config/app')
+const bcrypt = require('bcrypt')
+const MongoHelper = require('../../infra/helpers/mongo-connection-helper')
+let userModel
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(globalThis.__MONGO_URI__, globalThis.__MONGO_DB_NAME__)
+    userModel = await MongoHelper.getCollection('users')
+  })
+
+  beforeEach(async () => {
+    await userModel.deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return 200 if valid credentials are provided', async () => {
+    await userModel.insertOne({
+      name: 'Any Name',
+      email: 'valid_email@mail.com',
+      password: bcrypt.hashSync('hashed_password', 10)
+    })
+
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'valid_email@mail.com',
+        password: 'hashed_password'
+      })
+      .expect(200)
+  })
+})
