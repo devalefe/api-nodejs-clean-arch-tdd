f419415c26a4ee9c911763e74fb78af3f051fc80
Alefe F Oliveira <dev.alefe@gmail.com>
2023-09-25 19:04:22 -0300

    refactor: fix test name

--- src/utils/validators/signup-validator.spec.js
+++ src/utils/validators/signup-validator.spec.js
@@ -2,7 +2,7 @@ const InvalidParamError = require('../errors/invalid-param-error')
 const SignUpValidator = require('./signup-validator')
 
 describe('SignUp Validator', () => {
-  test('Should throw if no firstName is provided', async () => {
+  test('Should throw if invalid values are provided', async () => {
     const validator = new SignUpValidator()
     const promise = validator.validate()
     await expect(promise).rejects.toThrow(new InvalidParamError('firstName is a required field,lastName is a required field,phone is a required field,email is a required field,password is a required field'))
