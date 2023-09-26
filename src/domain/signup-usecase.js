24d9458a8bfb495cf1314f8599e1c28ee5eca805
devalefe <dev.alefe@gmail.com>
2023-09-26 13:58:27 -0300

    refactor: rename SignUpUseCase.register props to 'userData'

--- src/domain/signup-usecase.js
+++ src/domain/signup-usecase.js
@@ -11,18 +11,18 @@ module.exports = class SignUpUseCase {
     this.createUserAccountRepository = createUserAccountRepository
   }
 
-  async register (props = {}) {
+  async register (userData = {}) {
     const requiredFields = ['firstName', 'lastName', 'phone', 'email', 'password']
     for (const field of requiredFields) {
-      if (!props[field]) {
+      if (!userData[field]) {
         throw new MissingParamError(`Missing param: ${field}`)
       }
     }
-    const emailAlreadyExists = await this.loadUserByEmailRepository.load(props.email)
+    const emailAlreadyExists = await this.loadUserByEmailRepository.load(userData.email)
     if (emailAlreadyExists) {
       throw new InvalidParamError('Invalid param: email already in use')
     }
-    const hashedPassword = await this.encrypter.hash(props.password)
-    await this.createUserAccountRepository.save(Object.assign({}, props, { password: hashedPassword }))
+    const hashedPassword = await this.encrypter.hash(userData.password)
+    await this.createUserAccountRepository.save(Object.assign({}, userData, { password: hashedPassword }))
   }
 }
