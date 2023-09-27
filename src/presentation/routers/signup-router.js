ddd1aabcd55aa7c09f623d21fe58a1f4de3319ba
devalefe <dev.alefe@gmail.com>
2023-09-27 15:22:04 -0300

    feat: ensure SignUpRouter calls SignUpUseCase with correct params

--- src/presentation/routers/signup-router.js
+++ src/presentation/routers/signup-router.js
@@ -2,15 +2,22 @@ const MissingParamError = require('../../utils/errors/missing-param-error')
 const HttpResponse = require('../helpers/http-response')
 
 module.exports = class SignUpRouter {
+  constructor ({
+    signUpUseCase
+  } = {}) {
+    this.signUpUseCase = signUpUseCase
+  }
+
   async route (httpRequest) {
     try {
-      const accountFormData = httpRequest.body
+      const accountData = httpRequest.body
       const requiredFields = ['firstName', 'lastName', 'phone', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
-        if (!accountFormData[field]) {
+        if (!accountData[field]) {
           return HttpResponse.badRequest(new MissingParamError(`${field} is a required field`))
         }
       }
+      await this.signUpUseCase.register(accountData)
     } catch (error) {
       return HttpResponse.serverError()
     }
