120cdf6ddf813eddafc659f96a32e935fd158925
devalefe <dev.alefe@gmail.com>
2023-09-28 12:42:03 -0300

    feat: add reouter composer and hashSalt

---
+++ src/main/composers/signup-router-composer.js
@@ -0,0 +1,29 @@
+const SignUpUseCase = require('../../domain/signup-usecase')
+const SignUpRouter = require('../../presentation/routers/signup-router')
+const TokenGenerator = require('../../utils/helpers/token-generator')
+const Encrypter = require('../../utils/helpers/encrypter')
+const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
+const CreateAccountRepository = require('../../infra/repositories/create-account-repository')
+const UpdateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository')
+const { tokenSecret, hashSalt } = require('../config/env')
+
+module.exports = class SignUpRouterComposer {
+  static compose () {
+    const encrypter = new Encrypter(hashSalt)
+    const tokenGenerator = new TokenGenerator(tokenSecret)
+    const loadUserByEmailRepository = new LoadUserByEmailRepository()
+    const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+    const createAccountRepository = new CreateAccountRepository()
+    const signUpUseCase = new SignUpUseCase({
+      loadUserByEmailRepository,
+      updateAccessTokenRepository,
+      createAccountRepository,
+      tokenGenerator,
+      encrypter
+    })
+    const signUpRouter = new SignUpRouter({
+      signUpUseCase
+    })
+    return signUpRouter
+  }
+}
--- src/main/config/env.js
+++ src/main/config/env.js
@@ -2,5 +2,6 @@ module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://dev:dev@localhost:27017',
   dbName: 'e_prester',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
+  hashSalt: 12,
   port: process.env.PORT || 5050
 }
