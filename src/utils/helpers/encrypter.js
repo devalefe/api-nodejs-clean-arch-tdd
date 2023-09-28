4ca87eab4029573f0ae2fd7b49bbb4ecb48d2f0b
devalefe <dev.alefe@gmail.com>
2023-09-28 12:20:30 -0300

    feature: add hash method

--- src/utils/helpers/encrypter.js
+++ src/utils/helpers/encrypter.js
@@ -2,6 +2,10 @@ const bcrypt = require('bcrypt')
 const MissingParamError = require('../errors/missing-param-error')
 
 module.exports = class Encrypter {
+  constructor (salt = 12) {
+    this.salt = salt
+  }
+
   async compare (value, hash) {
     if (!value) {
       throw new MissingParamError('value')
@@ -11,5 +15,16 @@ module.exports = class Encrypter {
     }
     const valueMatch = await bcrypt.compare(value, hash)
     return valueMatch
+  }
+
+  async hash (value) {
+    if (!value) {
+      throw new MissingParamError('value')
+    }
+    if (!this.salt) {
+      throw new MissingParamError('salt')
+    }
+    const valueHashed = await bcrypt.hash(value, this.salt)
+    return valueHashed
   }
 }
