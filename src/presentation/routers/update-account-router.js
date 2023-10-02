fc57fc5005ba5cabb70a3892c6c67041cac3ae98
devalefe <dev.alefe@gmail.com>
2023-10-02 18:47:09 -0300

    chore: add UpdateAccountValidator

--- src/presentation/routers/update-account-router.js
+++ src/presentation/routers/update-account-router.js
@@ -2,16 +2,19 @@ const HttpResponse = require('../helpers/http-response')
 
 module.exports = class UpdateAccountRouter {
   constructor ({
-    updateAccountUseCase
+    updateAccountUseCase,
+    updateAccountValidator
   } = {}) {
     this.updateAccountUseCase = updateAccountUseCase
+    this.updateAccountValidator = updateAccountValidator
   }
 
   async route (httpRequest) {
     try {
       const formData = httpRequest.body
       if (!formData) throw new Error()
-      await this.updateAccountUseCase.update(formData)
+      const accountData = await this.updateAccountValidator.validate(formData)
+      await this.updateAccountUseCase.update(accountData)
     } catch (error) {
       return HttpResponse.serverError()
     }
