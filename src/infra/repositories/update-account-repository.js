1c80c122f16a797af8af3ad4ee0b783253ebe7bf
devalefe <dev.alefe@gmail.com>
2023-10-02 15:50:02 -0300

    feat: add UpdateAccountRepository

---
+++ src/infra/repositories/update-account-repository.js
@@ -0,0 +1,24 @@
+const { MissingParamError } = require('../../utils/errors')
+const MongoHelper = require('../helpers/mongo-connection-helper')
+
+module.exports = class UpdateAccountRepository {
+  async update (accountId, accountData) {
+    if (!accountId) {
+      throw new MissingParamError('id')
+    }
+    if (!accountData) {
+      throw new MissingParamError('accountData')
+    }
+    const accountModel = await MongoHelper.getCollection('users')
+    await accountModel.updateOne({
+      _id: accountId
+    }, {
+      $set: {
+        firstName: accountData.firstName,
+        lastName: accountData.lastName,
+        phone: accountData.phone,
+        email: accountData.email
+      }
+    })
+  }
+}
