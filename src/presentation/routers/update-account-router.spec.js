43b1cfc359dc95a1576978d341b62a6a1ebfbf84
devalefe <dev.alefe@gmail.com>
2023-10-02 18:24:35 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/routers/update-account-router.spec.js
+++ src/presentation/routers/update-account-router.spec.js
@@ -12,16 +12,23 @@ class UpdateAccountRouter {
   }
 }
 
+const makeSut = () => {
+  const sut = new UpdateAccountRouter()
+  return {
+    sut
+  }
+}
+
 describe('UpdateAccount Router', () => {
   test('Should return 500 if no httpRequest is provided', async () => {
-    const sut = new UpdateAccountRouter()
+    const { sut } = makeSut()
     const httpResponse = await sut.route()
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body.message).toBe(new ServerError().message)
   })
 
   test('Should return 500 if no formData is provided', async () => {
-    const sut = new UpdateAccountRouter()
+    const { sut } = makeSut()
     const httpRequest = {
       body: undefined
     }
