5abd4b13a6763445848c419f7977b3dcf74c8ebb
devalefe <dev.alefe@gmail.com>
2023-10-02 13:45:39 -0300

    test: ensure UpdateProfileUseCase.update return null if profile has updated successfuly

---
+++ src/domain/update-profile-usecase.spec.js
@@ -0,0 +1,17 @@
+const UpdateProfileUseCase = require('./update-profile-usecase')
+
+const signUpForm = {
+  firstName: 'John',
+  lastName: 'Doe',
+  phone: '+55 00 0000-0000',
+  email: 'test@mail.com',
+  password: 'TestUpperLower1'
+}
+
+describe('Update Profile UseCase', () => {
+  test('Should return null if account has updated successfuly', async () => {
+    const sut = new UpdateProfileUseCase()
+    const result = await sut.update(signUpForm)
+    expect(result).toBeNull()
+  })
+})
