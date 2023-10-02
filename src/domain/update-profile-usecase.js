886564a19ae8bb4af8fb5d965b24bd4e063c0fa2
devalefe <dev.alefe@gmail.com>
2023-10-02 14:20:26 -0300

    feat: add id validation

--- src/domain/update-profile-usecase.js
+++ src/domain/update-profile-usecase.js
@@ -1,5 +1,11 @@
+const { MissingParamError } = require('../utils/errors')
+
 module.exports = class UpdateProfileUseCase {
   async update (userData = {}) {
+    if (!userData.id) {
+      throw new MissingParamError('id')
+    }
+
     return true
   }
 }
