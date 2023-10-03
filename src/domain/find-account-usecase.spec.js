c0c4ff4dcf39082954af3ad1f300f3757e252206
devalefe <dev.alefe@gmail.com>
2023-10-03 13:46:22 -0300

    test: ensure FindAccountUseCase throw if no accountId is provided

--- src/domain/find-account-usecase.spec.js
+++ src/domain/find-account-usecase.spec.js
@@ -1,5 +1,11 @@
+const { MissingParamError } = require('../utils/errors')
+
 class FindAccountUseCase {
-  async find (accountId) {}
+  async find (accountId) {
+    if (!accountId) {
+      throw new MissingParamError('accountId')
+    }
+  }
 }
 
 describe('ViewAccount Use Case', () => {
@@ -8,5 +14,11 @@ describe('ViewAccount Use Case', () => {
     const sutSpy = jest.spyOn(sut, 'find')
     await sut.find('any_id')
     expect(sutSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should throw if no accountId is provided', async () => {
+    const sut = new FindAccountUseCase()
+    const promise = sut.find()
+    await expect(promise).rejects.toThrow(new MissingParamError('accountId'))
   })
 })
