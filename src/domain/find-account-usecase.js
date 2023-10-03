319a92103a319ba6c45cfb5ff8def9c7b80a6625
devalefe <dev.alefe@gmail.com>
2023-10-03 14:07:50 -0300

    feat: add FindAccountUseCase

---
+++ src/domain/find-account-usecase.js
@@ -0,0 +1,17 @@
+const { MissingParamError } = require('../utils/errors')
+
+module.exports = class FindAccountUseCase {
+  constructor ({
+    findAccountByIdRepository
+  } = {}) {
+    this.findAccountByIdRepository = findAccountByIdRepository
+  }
+
+  async find (accountId) {
+    if (!accountId) {
+      throw new MissingParamError('accountId')
+    }
+    const account = await this.findAccountByIdRepository.find(accountId)
+    return account
+  }
+}
