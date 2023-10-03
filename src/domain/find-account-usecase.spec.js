0228a613c78466487802d48e6b39774c57440603
devalefe <dev.alefe@gmail.com>
2023-10-03 13:51:34 -0300

    refactor: move sut creation to a factory helper method

--- src/domain/find-account-usecase.spec.js
+++ src/domain/find-account-usecase.spec.js
@@ -8,16 +8,23 @@ class FindAccountUseCase {
   }
 }
 
+const makeSut = () => {
+  const sut = new FindAccountUseCase()
+  return {
+    sut
+  }
+}
+
 describe('ViewAccount Use Case', () => {
   test('Should call FindAccountUseCase.find with correct id', async () => {
-    const sut = new FindAccountUseCase()
+    const { sut } = makeSut()
     const sutSpy = jest.spyOn(sut, 'find')
     await sut.find('any_id')
     expect(sutSpy).toHaveBeenCalledWith('any_id')
   })
 
   test('Should throw if no accountId is provided', async () => {
-    const sut = new FindAccountUseCase()
+    const { sut } = makeSut()
     const promise = sut.find()
     await expect(promise).rejects.toThrow(new MissingParamError('accountId'))
   })
