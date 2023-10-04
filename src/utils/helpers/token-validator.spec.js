0dc7b3b05b894ccd00b33e0ba3867a65989848f6
devalefe <dev.alefe@gmail.com>
2023-10-04 20:23:49 -0300

    test: ensure TokenValidator throws if no secret is provided

--- src/utils/helpers/token-validator.spec.js
+++ src/utils/helpers/token-validator.spec.js
@@ -16,7 +16,14 @@ const jwt = require('jsonwebtoken')
 const MissingParamError = require('../errors/missing-param-error')
 
 class TokenValidator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
   async validate (token) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
     if (!token) {
       throw new MissingParamError('token')
     }
@@ -36,6 +43,12 @@ describe('Token Validator', () => {
     const { sut } = makeSut()
     await sut.validate('any_token')
     expect(jwt.token).toBe('any_token')
+  })
+
+  test('Should throw if no secret is provided', async () => {
+    const sut = new TokenValidator()
+    const promise = sut.validate('any_token')
+    await expect(promise).rejects.toThrow(new MissingParamError('secret'))
   })
 
   test('Should throw if no token is provided', async () => {
