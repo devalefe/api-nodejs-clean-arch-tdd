68407e7429310be659fde853466544be099285aa
devalefe <dev.alefe@gmail.com>
2023-10-04 20:20:05 -0300

    test: ensure TokenValidator throw if no token is provided

--- src/utils/helpers/token-validator.spec.js
+++ src/utils/helpers/token-validator.spec.js
@@ -13,9 +13,13 @@ jest.mock('jsonwebtoken', () => ({
 }))
 
 const jwt = require('jsonwebtoken')
+const MissingParamError = require('../errors/missing-param-error')
 
 class TokenValidator {
   async validate (token) {
+    if (!token) {
+      throw new MissingParamError('token')
+    }
     return jwt.verify(token)
   }
 }
@@ -25,5 +29,11 @@ describe('Token Validator', () => {
     const sut = new TokenValidator()
     await sut.validate('any_token')
     expect(jwt.token).toBe('any_token')
+  })
+
+  test('Should throw if no token is provided', async () => {
+    const sut = new TokenValidator()
+    const promise = sut.validate()
+    await expect(promise).rejects.toThrow(new MissingParamError('token'))
   })
 })
