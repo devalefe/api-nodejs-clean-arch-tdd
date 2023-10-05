780166bc18be09681746ed0f2308b94ce3df2cf8
devalefe <dev.alefe@gmail.com>
2023-10-05 15:15:35 -0300

    refactor: add auth token support

--- src/main/routes/account-routes.test.js
+++ src/main/routes/account-routes.test.js
@@ -1,7 +1,9 @@
 const request = require('supertest')
 const app = require('../config/app')
 const MongoHelper = require('../../infra/helpers/mongo-connection-helper')
-let userModel, accountId
+const TokenGenerator = require('../../utils/helpers/token-generator')
+const { tokenSecret } = require('../config/env')
+let userModel, authToken
 
 const updateAccountForm = {
   firstName: 'John',
@@ -14,28 +16,24 @@ describe('UpdateAccount Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(globalThis.__MONGO_URI__, globalThis.__MONGO_DB_NAME__)
     userModel = await MongoHelper.getCollection('users')
-  })
-
-  beforeEach(async () => {
     const { insertedId } = await userModel.insertOne(updateAccountForm)
-    accountId = insertedId
-  })
-
-  afterEach(async () => {
-    await userModel.deleteMany()
+    const tokenGenerator = new TokenGenerator(tokenSecret)
+    authToken = await tokenGenerator.generate(insertedId)
   })
 
   afterAll(async () => {
+    await userModel.deleteMany()
     await MongoHelper.disconnect()
   })
 
   test('Should return 200 if valid credentials are provided', async () => {
     const updatedAccount = Object.assign(
       {}, updateAccountForm,
-      { id: accountId, firstName: 'Jane' }
+      { firstName: 'Jane' }
     )
     const response = await request(app)
-      .patch('/api/account')
+      .put('/api/account')
+      .auth(authToken, { type: 'bearer' })
       .send(updatedAccount)
     expect(response.statusCode).toBe(200)
     expect(response.body.message).toBe('Sucesso ao atualizar')
@@ -45,10 +43,11 @@ describe('UpdateAccount Routes', () => {
     delete updateAccountForm._id
     for (const field of Object.keys(updateAccountForm)) {
       const response = await request(app)
-        .patch('/api/account')
+        .put('/api/account')
+        .auth(authToken, { type: 'bearer' })
         .send(Object.assign(
           {}, updateAccountForm,
-          { id: accountId, [field]: undefined }
+          { [field]: undefined }
         ))
       expect(response.statusCode).toBe(400)
       expect(response.body.message).toBe('Falha ao validar os campos')
@@ -65,10 +64,11 @@ describe('UpdateAccount Routes', () => {
     })
     const updatedAccount = Object.assign(
       {}, updateAccountForm,
-      { id: accountId, email: 'jane@mail.com' }
+      { email: 'jane@mail.com' }
     )
     const response = await request(app)
-      .patch('/api/account')
+      .put('/api/account')
+      .auth(authToken, { type: 'bearer' })
       .send(updatedAccount)
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual({
