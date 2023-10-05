6fe788d54d01e44b595f4541d85b1c1773a89ebb
devalefe <dev.alefe@gmail.com>
2023-10-05 15:11:49 -0300

    refactor: add env variables

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://dev:devpass@localhost:27017',
-  dbName: 'e_prester',
+  dbName: process.env.DB_NAME || 'e_prester',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
-  hashSalt: 12,
+  hashSalt: process.env.HASH_SALT || 12,
   port: process.env.PORT || 5050
 }
