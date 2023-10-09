3a7a7aa3d06f06ec09c8d12dede3c7f06485a9af
Alefe F Oliveira <dev.alefe@gmail.com>
2023-10-08 21:51:56 -0300

    test: ensure FindAccountRouter return 401 if invalid token is provided

--- src/presentation/routers/find-account-router.spec.js
+++ src/presentation/routers/find-account-router.spec.js
@@ -1,5 +1,5 @@
 const { MissingParamError } = require('../../utils/errors')
-const { ServerError } = require('../errors')
+const { ServerError, UnauthorizedError } = require('../errors')
 const HttpResponse = require('../helpers/http-response')
 
 class FindAccountRouter {
@@ -29,8 +29,28 @@ class FindAccountRouter {
           message: 'Usuário não autenticado'
         })
       }
+      if (
+        error.name === 'JsonWebTokenError' ||
+        error.name === 'TokenExpiredError'
+      ) {
+        return HttpResponse.unauthorizedError()
+      }
       return HttpResponse.serverError()
     }
+  }
+}
+
+class JsonWebTokenErrorStub extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'JsonWebTokenError'
+  }
+}
+
+class TokenExpiredErrorStub extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'TokenExpiredError'
   }
 }
 
@@ -112,6 +132,26 @@ describe('FindAccount Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body.message).toBe('Usuário não autenticado')
+  })
+
+  test('Should return 401 if invalid token is provided', async () => {
+    const httpRequest = {
+      headers: httpHeaders
+    }
+    const { sut, tokenValidator } = makeSut()
+    const stubs = [
+      new JsonWebTokenErrorStub(),
+      new TokenExpiredErrorStub()
+    ]
+    for (const stub of stubs) {
+      jest.spyOn(tokenValidator, 'validate')
+        .mockReturnValueOnce(new Promise((resolve, reject) =>
+          reject(stub)
+        ))
+      const httpResponse = await sut.route(httpRequest)
+      expect(httpResponse.statusCode).toBe(401)
+      expect(httpResponse.body.message).toBe(new UnauthorizedError().message)
+    }
   })
 
   test('Should return 500 if no httpRequest is provided', async () => {
