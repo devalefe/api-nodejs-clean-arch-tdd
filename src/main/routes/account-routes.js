5e5ba5c4fb431e0d073f565d1156959fd95aa264
Alefe F Oliveira <dev.alefe@gmail.com>
2023-10-10 14:06:22 -0300

    feat: add get account router

--- src/main/routes/account-routes.js
+++ src/main/routes/account-routes.js
@@ -1,7 +1,10 @@
 const ExpressRouterAdapter = require('../adapters/express-router-adapter')
+const FindAccountRouterComposer = require('../composers/find-account-router-composer')
 const UpdateAccountRouterComposer = require('../composers/update-account-router-composer')
 
 module.exports = (router) => {
+  const findAccountRouter = FindAccountRouterComposer.compose()
   const updateAccountRouter = UpdateAccountRouterComposer.compose()
+  router.get('/account', ExpressRouterAdapter.adapt(findAccountRouter))
   router.put('/account', ExpressRouterAdapter.adapt(updateAccountRouter))
 }
