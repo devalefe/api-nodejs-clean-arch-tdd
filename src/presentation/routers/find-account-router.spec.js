f7963dc3dc7b7dc182f794122d79c0f2120492ab
Alefe F Oliveira <dev.alefe@gmail.com>
2023-10-10 12:53:48 -0300

    refactor: move FindAccountRouter to its own file

--- src/presentation/routers/find-account-router.spec.js
+++ src/presentation/routers/find-account-router.spec.js
@@ -1,50 +1,5 @@
-const { MissingParamError } = require('../../utils/errors')
 const { ServerError, UnauthorizedError } = require('../errors')
-const HttpResponse = require('../helpers/http-response')
-
-class FindAccountRouter {
-  constructor ({
-    findAccountUseCase,
-    tokenValidator
-  } = {}) {
-    this.findAccountUseCase = findAccountUseCase
-    this.tokenValidator = tokenValidator
-  }
-
-  async route (httpRequest) {
-    try {
-      const { headers } = httpRequest
-      if (!headers) {
-        throw new MissingParamError('headers')
-      }
-      if (!headers.authorization) {
-        throw new MissingParamError('token')
-      }
-      const { id } = await this.tokenValidator.validate(headers.authorization)
-      const account = await this.findAccountUseCase.find(id)
-      return HttpResponse.ok({
-        message: 'Sucesso ao buscar conta',
-        account
-      })
-    } catch (error) {
-      if (
-        error.name === 'MissingParamError' &&
-        error.message !== 'headers'
-      ) {
-        return HttpResponse.badRequest({
-          message: 'Usuário não autenticado'
-        })
-      }
-      if (
-        error.name === 'JsonWebTokenError' ||
-        error.name === 'TokenExpiredError'
-      ) {
-        return HttpResponse.unauthorizedError()
-      }
-      return HttpResponse.serverError()
-    }
-  }
-}
+const FindAccountRouter = require('./find-account-router')
 
 class JsonWebTokenErrorStub extends Error {
   constructor (message) {
