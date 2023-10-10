a4a34288351482ca187dec7c74d58ccb9c3ed9db
Alefe F Oliveira <dev.alefe@gmail.com>
2023-10-10 12:53:25 -0300

    feat: add FindAccountRouter

---
+++ src/presentation/routers/find-account-router.js
@@ -0,0 +1,46 @@
+const { MissingParamError } = require('../../utils/errors')
+const HttpResponse = require('../helpers/http-response')
+
+module.exports = class FindAccountRouter {
+  constructor ({
+    findAccountUseCase,
+    tokenValidator
+  } = {}) {
+    this.findAccountUseCase = findAccountUseCase
+    this.tokenValidator = tokenValidator
+  }
+
+  async route (httpRequest) {
+    try {
+      const { headers } = httpRequest
+      if (!headers) {
+        throw new MissingParamError('headers')
+      }
+      if (!headers.authorization) {
+        throw new MissingParamError('token')
+      }
+      const { id } = await this.tokenValidator.validate(headers.authorization)
+      const account = await this.findAccountUseCase.find(id)
+      return HttpResponse.ok({
+        message: 'Sucesso ao buscar conta',
+        account
+      })
+    } catch (error) {
+      if (
+        error.name === 'MissingParamError' &&
+        error.message !== 'headers'
+      ) {
+        return HttpResponse.badRequest({
+          message: 'Usuário não autenticado'
+        })
+      }
+      if (
+        error.name === 'JsonWebTokenError' ||
+        error.name === 'TokenExpiredError'
+      ) {
+        return HttpResponse.unauthorizedError()
+      }
+      return HttpResponse.serverError()
+    }
+  }
+}
