0a9cd7fe74bd2a879741a8ae6a6828792c757a29
Alefe F Oliveira <dev.alefe@gmail.com>
2023-10-10 13:33:39 -0300

    refactor: move LoadUserByIdRepository to its own file

--- src/infra/repositories/account/find.spec.js
+++ src/infra/repositories/account/find.spec.js
@@ -1,22 +1,7 @@
 const { MissingParamError } = require('../../../utils/errors')
 const MongoHelper = require('../../helpers/mongo-connection-helper')
+const LoadUserByIdRepository = require('./find')
 let userModel
-
-class LoadUserByIdRepository {
-  async load (id) {
-    if (!id) {
-      throw new MissingParamError('id')
-    }
-    const userModel = await MongoHelper.getCollection('users')
-    const user = await userModel.findOne({ _id: id })
-    if (user) {
-      const parsedData = Object.assign({}, user, { id: user._id.toString() })
-      delete parsedData._id
-      return parsedData
-    }
-    return null
-  }
-}
 
 describe('LoadUserByID Repository', () => {
   beforeAll(async () => {
