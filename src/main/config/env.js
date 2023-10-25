435d81784de149cdd3fb91058208b220aa42e31e
Alefe F Oliveira <dev.alefe@gmail.com>
2023-10-25 13:12:14 -0300

    bugfix: add number conversion to hashSalt and port from env config

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -2,6 +2,6 @@ module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://dev:devpass@localhost:27017',
   dbName: process.env.DB_NAME || 'e_prester',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
-  hashSalt: process.env.HASH_SALT || 12,
-  port: process.env.PORT || 5050
+  hashSalt: Number(process.env.HASH_SALT) || 12,
+  port: Number(process.env.PORT) || 5050
 }
